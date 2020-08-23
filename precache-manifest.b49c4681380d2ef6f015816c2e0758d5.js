018baa1e436e35165ce673c6eb724fe690b02fb3
rajdavid <47979488+rajdavid@users.noreply.github.com>
2020-08-24 00:32:57 +0530

    Updates

---
+++ precache-manifest.b49c4681380d2ef6f015816c2e0758d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a56f0e0b83d20b40a559039b3b19ece",
+    "url": "/learn-react/index.html"
+  },
+  {
+    "revision": "22538acfb2ea0b6bc118",
+    "url": "/learn-react/static/css/main.edb3526b.chunk.css"
+  },
+  {
+    "revision": "122179f5a4afa75e2459",
+    "url": "/learn-react/static/js/2.6a25ef39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learn-react/static/js/2.6a25ef39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22538acfb2ea0b6bc118",
+    "url": "/learn-react/static/js/main.5d7e4225.chunk.js"
+  },
+  {
+    "revision": "bb9adc4ecf8cb73e24c2",
+    "url": "/learn-react/static/js/runtime-main.96e9939a.js"
+  }
+]);
