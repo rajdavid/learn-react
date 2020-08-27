09b0ab29ac7ffb56eb8e65974654b31807a70d59
rajdavid <47979488+rajdavid@users.noreply.github.com>
2020-08-27 21:14:15 +0530

    functional component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import Counter from './Componets/Counter'
+import Greet from './Componets/Greet';
 function App() {
   return (
     <div className="App">
       hello to new world
       <hr />
       <br/>
-      <Counter/>
+      <Greet/>
+      <Counter />
+      
     </div>
   );
 }
---
+++ src/Componets/Greet.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// old way of writing functional compenents
+
+//  function Greet() {
+//     return (
+//         <div>
+//             <h1>Hey User</h1>
+//         </div>
+//     )
+// }
+// export default Greet;
+
+
+// es6 way of writing functional component
+//using arrow function
+const Greet = () => <h1>Click Counter ...</h1>
+export default Greet
